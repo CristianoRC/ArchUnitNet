e732b238b53bbc95aea845de9568972f73f5736b
Cristiano Cunha <contato@cristianoprogramador.com>
2025-03-04 17:23:49 -0300

    Cria testes de repository

---
+++ ProductExample.Core/IRepository.cs
@@ -0,0 +1,10 @@
+namespace ProductExample.Core;
+
+public interface IRepository
+{
+}
+
+//TODO: Remover comentários para testes de nomes
+/*public interface Repository
+{
+}*/
---
+++ ProductExample.UnitTest/InterfaceNameUnitTest.cs
@@ -0,0 +1,18 @@
+using ArchUnitNET.Domain;
+using ArchUnitNET.xUnit;
+using static ArchUnitNET.Fluent.ArchRuleDefinition;
+
+namespace ProductExample.UnitTest;
+
+public class InterfaceNameUnitTest : BaseArchUnitTest
+{
+    [Fact]
+    public void Interfaces_ShouldStartWith_LetterI()
+    {
+        var rule = Interfaces()
+            .Should().HaveNameStartingWith("I");
+
+        rule.Because("Todas as interfaces devem começar com a letra 'I' para seguir as convenções de nomenclatura")
+            .Check(DynamicArchitecture);
+    }
+}
